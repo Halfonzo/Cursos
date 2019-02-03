99b8974886fece8b642604254b7f8d99298f27e1
halfonso <1630065@upv.edu.mx>
2019-02-03 06:43:23 +0000

    Domingo

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2019-02-03 04:28:16 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2019-02-03T04:28:16+00:00	INFO 189.218.223.228	joomlafailure	Empty password not allowed.
